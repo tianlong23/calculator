d0e4296c7bafa60ca036ad9bd9057da559359736
tianlong23 <longch23@gmail.com>
2022-12-15 06:53:34 +0800

    Add index, style, and script2 in final version, fix bugs  Please enter the commit message for your changes. Lines starting

--- script2.js
+++ script2.js
@@ -1,3 +1,5 @@
+//Global Variables
+
 var displayArray = [];
 var symbols = ['+', "-", "/", "x"]
 
@@ -8,15 +10,103 @@ var tempOperator = [];
 var solution = 0;
 var operatorValue = '';
 
+//Function to check the location of the operator in the string
+
 function checkOperator() {
-var containAll = symbols.some(item => {
+if (displayArray[0] == '-'){
+    var displayArray2 = [];
+    displayArray2 = displayArray.slice(1)
+    var containAll = symbols.some(item => {
+        index = displayArray2.indexOf(item);
+        return containAll = displayArray2.indexOf(item) !== -1;
+    });
+} else {
+
+    var containAll = symbols.some(item => {
     index = displayArray.indexOf(item);
     return containAll = displayArray.indexOf(item) !== -1;
 });
-console.log('contain all: '+ containAll)
+}
 }
 
+//Function to trigger the CSS to fire an animation when an operator is clicked. Adds to the current click, removes all others
+
+highlightAction();
+
+function highlightAction() {
+    const button = document.getElementsByClassName('action');
+    for (let i = 0; i<button.length; i++) {
+        button[i].addEventListener('click', function() {
+            for( let x = 0; x<button.length; x++) {
+                button[x].classList.remove('highlight');
+            }
+            button[i].classList.add('highlight');
+        })
+    }
+}
+
+//Function to trigger a quick flash when a button is clicked in the CSS
+
+numberFlash();
+
+function numberFlash() {
+    const button = document.getElementsByClassName('number');
+    for (let i = 0; i<button.length; i++) {
+        button[i].addEventListener('click', function() {
+            button[i].classList.add('numberHighlight');
+            setTimeout(() => button[i].classList.remove('numberHighlight'),400);
+        })
+    }
+}
+
+//Function to trigger CSS to get the equal button to flash when it is clicked
+
+clearEqual();
+
+function clearEqual() {
+    const button = document.getElementById('equals');
+    button.addEventListener('click', function() {
+        button.setAttribute('id', 'equalAction');
+        setTimeout(() => button.setAttribute('id','equals'),400);
+})
+}
+
+//Function to trigger CSS to get the clear button to flash when it is clicked
+
+clearFlash();
+
+function clearFlash() {
+    const button = document.getElementById('clear');
+        button.addEventListener('click', function() {
+            const buttonAction = document.getElementsByClassName('action');
+                            for( let x = 0; x<buttonAction.length; x++) {
+                                buttonAction[x].classList.remove('highlight');
+                            }
+            
+            button.classList.add('clearHighlight');
+            setTimeout(() => button.classList.remove('clearHighlight'),400);
+        })
+    }
+
+//Function to get the whole calculator to clear on click
+
+function clear() {
+    const button = document.getElementById("clear");
+    button.addEventListener('click', function() {
+        displayArray = [];
+        resetValues();
+        const display = document.getElementById("displayCase");
+        display.textContent = "0";
+    })
+
+}
+
+clear();
+
 addNumbersA();
+
+//Function to add numbers, sets the b value ongoing by putting them in an array. a values are only added if they are missing (i.e. start with an operator)
+//function also triggers the equal button when clicked, including catches for some errors such as a = NaN.
 
 function addNumbersA() {
     const button = document.getElementsByClassName("number");
@@ -29,14 +119,37 @@ function addNumbersA() {
                 checkOperator(displayArray);
                 b = displayArray.slice(index + 1,displayArray.length);
                 b = b.join('');
-                b = parseInt(b);
+                b = parseFloat(b);
                 const equalButton = document.getElementById("equals");
-                        equalButton.addEventListener("click", function() {
+                    equalButton.addEventListener("click", function() {
                             if (a == 0) {
+                                a = 0;
+                                operatorValue = displayArray.at(-1);
+                                checkOperator(displayArray);
+                                b = displayArray.slice(index + 1,displayArray.length);
+                                b = b.join('');
+                                b = parseFloat(b);
+                                selectOperator(operatorValue);
                                 return;
-                            }
+                            } else if (a<0 && b <0 && operatorValue == '-') {
+                                doubleSubtract(a,b)
+                                return;
+                            } else if (isNaN(a) == true) {
+                                a = 0;
+                                operatorValue = displayArray.at(0);
+                                b = displayArray.slice(1,displayArray.length);
+                                b = b.join('');
+                                b = parseFloat(b);
+                                selectOperator(operatorValue);
+                            } else {
                             selectOperator(operatorValue);
+                            const buttonAction = document.getElementsByClassName('action');
+
+                            for( let x = 0; x<buttonAction.length; x++) {
+                                buttonAction[x].classList.remove('highlight');
+                            }
                             return;
+                        }
                         })
                 return;
         });
@@ -45,17 +158,18 @@ function addNumbersA() {
 
 addOperators();
 
+//Function to add the operator to the array. When an operator is selected. The a value is set by splicing the array. 
+//B values continue to be added as more numbers are clicked
+//If a and b values are present, and another operator is clicked, it does the math, and sets the most recent operator as the new operator. 
+//A value is set as the solution in below math functions. 
+
 function addOperators() {
     const button = document.getElementsByClassName("action");
     for (let i = 0; i < button.length; i++) {
         button[i].addEventListener('click', function() {
             if (a>0 && b>0) {
-                console.log('tempoperator: '+ tempOperator)
                 tempOperator.pop();
                 tempOperator.push(button[i].textContent);
-                console.log('temp operator: '+ tempOperator)
-
-                console.log('if operatorvalue: '+ operatorValue)
                 selectOperatorTwo(operatorValue);
                 return ;
             } else if (displayArray.at(-1) == "+" || displayArray.at(-1) == "-" || displayArray.at(-1) == "/" || displayArray.at(-1) == "x" ) {
@@ -67,16 +181,21 @@ function addOperators() {
             display.textContent = displayArray.join('');
             a = displayArray.slice(0,-1);
             a = a.join('');
-            a = parseInt(a);
-            console.log('post else if operatorvalue: '+ operatorValue)
+            a = parseFloat(a);
+            if ( isNaN(a) == true) {
+                a = 0;
+                a = displayArray.slice(0,-1);
+                a = a.join('');
+                a = parseFloat(a);
+            }
             operatorValue = displayArray.at(-1);
         })
     }
 }
 
+//Function to identify what math should be done based on the operator. This function only works if another operator was chosen instead of the equal sign. 
+
 function selectOperatorTwo(stringop) {
-    console.log('switch2 stringop: '+ stringop)
-    console.log('switch2 operatorvalue: '+ operatorValue)
     switch (stringop) {
         case "+":
             addTwo(a,b, stringop)
@@ -93,8 +212,9 @@ function selectOperatorTwo(stringop) {
     }
 }
 
+//Function to identify what math should be done when the equal sign is clicked. 
+
 function selectOperator(operatorValue) {
-    console.log('switch1 operatorvalue: '+ operatorValue)
     switch (operatorValue) {
         case "+":
             add(a,b)
@@ -112,14 +232,17 @@ function selectOperator(operatorValue) {
 }
 
 
-
+//Convert array to nubmer
 let toArray = num => Number(num);
 
+
+//reset values
 function resetValues() {
     a = 0;
     b = 0;
 }
 
+//Addition function, also resets the display array, a, and b values
 function add(a,b){
     displayArray = [];
     solution = a + b;
@@ -128,7 +251,7 @@ function add(a,b){
     resetValues(a,b);
     return;
 }
-
+//Subtract function, also resets the display array, a, and b values
 function subtract(a,b){
     const solution = a - b;
     const display = document.getElementById("displayCase");
@@ -137,7 +260,16 @@ function subtract(a,b){
     displayArray = [];
     return;
 }
-
+//Function uses + instead of -, only is triggered through an if statemnet in the addnumbers function. If b value is -.
+function doubleSubtract (a,b) {
+    const solution = a + b;
+    const display = document.getElementById("displayCase");
+    display.textContent = solution;
+    resetValues(a,b);
+    displayArray = [];
+    return;
+}
+//Division function, also resets the display array, a, and b values
 function divide(a,b){
     const solution = a / b;
     const display = document.getElementById("displayCase");
@@ -146,7 +278,7 @@ function divide(a,b){
     displayArray = [];
     return;
 }
-
+//Multiplication function, also resets the display array, a, and b values
 function multiply(a,b){
     const solution = a * b;
     const display = document.getElementById("displayCase");
@@ -156,21 +288,21 @@ function multiply(a,b){
     return;
 }
 
-
+//resets b values
 function resetB() {
     b = 0;
 }
 
+//converts a variable to the solution
 function solutionA(solution) {
     a = solution
 }
 
-
+//Addition function to be run if another operator is selected instead of the equal sign.
+//Takes the second operator and stores it, runs the math, and resets the array as the solution with the second operator applied. Resets b value
 function addTwo(a,b,stringop){
     stringop = tempOperator;
     stringop = stringop.join('');
-    console.log('add2 , 1stringop: '+ stringop)
-    console.log('add2 , 1operatorvalue: '+ operatorValue)
     solution = a + b;
     a = solution;
     const display = document.getElementById("displayCase");
@@ -178,20 +310,18 @@ function addTwo(a,b,stringop){
     displayArray = [];
     displayArray.push(solution);
     displayArray.push(stringop);
-    console.log('displayarray: '+ displayArray);
-    console.log('add2 , 2 stringop: '+ stringop)
-    console.log('add2  , 2operatorvalue: '+ operatorValue)
     operatorValue = displayArray.at(-1);
     solutionA(a);
     resetB(b);
     addOperators(a);
 }
 
+
+//Subtraction function to be run if another operator is selected instead of the equal sign.
+//Takes the second operator and stores it, runs the math, and resets the array as the solution with the second operator applied. Resets b value
 function subtractTwo(a,b,stringop){
     stringop = tempOperator;
     stringop = stringop.join('');
-    console.log('subtract2 , 1 stringop: '+ stringop)
-    console.log('subtract2 , 1 operatorvalue: '+ operatorValue)
     const solution = a - b;
     a = solution;
     const display = document.getElementById("displayCase");
@@ -199,23 +329,20 @@ function subtractTwo(a,b,stringop){
     displayArray = [];
     displayArray.push(solution);
     displayArray.push(stringop);
-    console.log('subtract2 , 2 stringop: '+ stringop)
-    console.log('subtract2 , 2 operatorvalue: '+ operatorValue)
-    console.log('math displayarray: '+ displayArray)
     operatorValue = displayArray.at(-1);
     solutionA(a);
     resetB(b);
     addOperators(a,displayArray);
 }
 
+//Division function to be run if another operator is selected instead of the equal sign.
+//Takes the second operator and stores it, runs the math, and resets the array as the solution with the second operator applied. Resets b value
 function divideTwo(a,b,stringop){
     stringop = tempOperator;
     stringop = stringop.join('');
     const solution = a / b;
     a = solution;
     const display = document.getElementById("displayCase");
-    console.log('solution: '+ solution)
-    console.log('typeof solution: '+ typeof solution)
     display.textContent = solution + stringop;
     displayArray = [];
     displayArray.push(solution);
@@ -226,6 +353,8 @@ function divideTwo(a,b,stringop){
     addOperators(a);
 }
 
+//Multiplication function to be run if another operator is selected instead of the equal sign.
+//Takes the second operator and stores it, runs the math, and resets the array as the solution with the second operator applied. Resets b value
 function multiplyTwo(a,b,stringop){
     stringop = tempOperator;
     stringop = stringop.join('');
